ffa0579825ec7ac5540e438d6fd39dd8bea8a951
Krishna Tanwar <kstanwar0000@gmail.com>
2025-03-16 10:44:18 +0530

    Intro to JS

---
+++ JavaScript/Video54/script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+
+console.log("Console is running");
+
+var a =prompt("Enter no");
+console.log("Number"+a);
+
+var istrue = confirm("Are you sure to leave?");
+if(istrue){
+    console.log("Computer is blasting");
+}
+else{
+    console.log("Not Blasting");
+}
+
+document.title = "Hey ";
+document.body.style.backgroundColor='red';
