39508acea207706a8a1accca0c9eb9a27304aecf
Krishna Tanwar <kstanwar0000@gmail.com>
2025-03-17 22:46:59 +0530

    Js Variables & Data types

---
+++ JavaScript/Video55/script.js
@@ -0,0 +1,48 @@
+console.log("Hey this is tutorial");
+
+// let a = 5;
+// var c = 10;
+// var b ="krishna";
+// var _a = 5;
+// var 55b = "krishna";//NOT ALLOWED
+// console.log(a+c);
+
+// console.log(a);
+// {
+//     let a = 66;
+//     console.log(a);
+// }
+// console.log(a);
+// console.log(typeof a, typeof b, typeof c);
+
+// const a1 = 5;
+// a1 +=1;// NOT ALLOWED
+
+
+// DATA TYPES
+
+// let x = "Krishna";
+// let y = 22;
+// let z = 2.23;
+// const p = true;
+// let q = undefined;
+// let r = null;
+
+// console.log(x,y,z,p,q,r);
+// console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r );
+
+
+// OBJECTS
+
+let o = {
+    name:"Krishna",
+    "Job code":5600,
+    "is_handsome":true
+
+}
+
+console.log(o);
+o.salary = "56000";
+console.log(o);
+o.salary = "50";
+console.log(o);
