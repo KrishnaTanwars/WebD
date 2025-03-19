a38c70625d7e3090d9a72cbf5c00782035eebc2a
Krishna Tanwar <kstanwar0000@gmail.com>
2025-03-19 15:41:20 +0530

    JavaScript Conditionals

---
+++ JavaScript/Video56/index.js
@@ -0,0 +1,25 @@
+// console.log("Hello this is krishna");
+
+let age = 18;
+// let grace = 2;
+// console.log(age+grace)
+// console.log(age-grace)
+// console.log(age*grace)
+// console.log(age/grace)
+// console.log(age**grace)
+// // console.log(age%grace)
+// if(age==18){
+//     console.log("Drive");
+// }
+// else if(age==0){
+//     console.log("Kidding?");
+// }
+// else {
+//     console.log("Not Drive");
+// }
+
+let a = 5;
+let b = 6;
+let c = b>a ? (b-a):(a-b);
+console.log(c);
+
---
+++ JavaScript/Video56/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+// console.log(age%grace)
+// if(age==18){
+//     console.log("Drive");
+// }
+// else if(age==0){
+//     console.log("Kidding?");
+// }
+// else {
+//     console.log("Not Drive");
+// }
