ff82611687fb97e66b5b150735f5960608858ccd
Krishna Tanwar <kstanwar0000@gmail.com>
2025-03-24 21:29:58 +0530

    Strings

---
+++ JavaScript/Video60/index.js
@@ -0,0 +1,35 @@
+console.log("Hi this is String tutorial")
+let a = "Krishna";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+
+console.log(a.length)
+
+let name = "Blue"
+let friend = "Aadi"
+console.log(`His name is ${name} and his friends name is ${friend}`)
+console.log('hi"kfs')
+console.log(`hi"kfs`)
+console.log("hi\"kfs")
+console.log("hi\nkfs")
+
+let b = 'kukaku'
+// console.log(b.toUpperCase())
+// console.log(b.toLowerCase())
+// console.log(b.slice(1,3))
+// console.log(b.slice(1))
+console.log(b.replace("kk","ld")) 
+console.log(b.concat(a,"Tanwar")) 
+console.log(b)
+
+console.log(b.charAt(3))
+console.log(b.indexOf("ku"))
+console.log(b.indexOf("sfd"))
+console.log(b.startsWith("ku"))
+console.log(b.endsWith("ar"))
