e58b74e4b263130f2bd707ba66ebea32bedbffd6
Krishna Tanwar <kstanwar0000@gmail.com>
2025-09-10 11:29:10 +0530

    Video 86 done

---
+++ Backend/Video 86/main.js
@@ -0,0 +1,12 @@
+// import {a,b,d} from './mymodule.js';
+// console.log(a,b,d)
+
+// import krish from './mymodule.js';
+// console.log(krish)
+
+const a = require('./mymodule2.js');
+console.log(a,__dirname,__filename);
+
+// function(exports,require,module,__filename,__dirname){
+//     //module code here
+// };
